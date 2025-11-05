1a7ae1d521be2e00db5247b9f2cf78d37d1c6dcd
Natali Queiróz <queiroznatt@gmail.com>
2025-11-05 16:17:22 -0400

    feat: Adicionar tratamento de entrada de nome de usuário e exibição de lista de opções

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const userNameInput = document.getElementById('user-name');
 const btnSaveUserName = document.getElementById('btn-save-user-name');
+const containerUserName = document.getElementById('container-user-name');
+const welcomeMessage = document.getElementById('welcome-message');
+// Elementos do DOM container tipo de lista
+const radioButtonTypeCompras = document.getElementById('option-type-compras');
+const radioButtonTypeRemedios = document.getElementById('option-type-remedios');
+const btnSaveTypeList = document.getElementById('btn-save-type-list');
+const containerChoiceTypeList = document.getElementById('container-choice-type-list');
 
 const shoppingList = [];
 let userName = ' ';
@@ -11,7 +18,10 @@ userNameInput.addEventListener('input', (event) => {
 btnSaveUserName.addEventListener('click', ()=>{
   if (userName.length > 2 && userName.length < 50){
       userNameInput.value = " ";
-      window.alert("Nome salvo com sucesso!")
+      window.alert("Nome salvo com sucesso!");
+      containerUserName.style.display = 'none';
+      containerChoiceTypeList.style.display = 'block';
+      welcomeMessage.textContent = `Boas vindas, ${userName}!`;
       return
   }
     window.alert("O nome deve ter entre 2 e 50 caracteres")
